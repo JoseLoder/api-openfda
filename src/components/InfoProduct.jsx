f39547314538f9f02cb291c8d9fe3264be2b44b0
MartinSilvaJose <martin.silva.jose@hotmail.com>
2025-01-10 17:56:58 +0100

    cuadrando la lista

--- src/components/InfoProduct.jsx
+++ src/components/InfoProduct.jsx
@@ -1,21 +1,58 @@
 /* eslint-disable react/prop-types */
+import { List, ListItemText, ListSubheader, Typography } from "@mui/material";
+import "../styles/InfoProduct.css";
+const tryShow = (string) => {
+  if (string) {
+    return string;
+  }
+  return "Sin aportaciones";
+}
 
 function InfoProduct({ product }) {
   return (
     <article className="card">
-<h2>{product.openfda?.brand_name}</h2>
-      <ul>
-        <li>Ingredientes Activos: {product.active_ingredient ? product.active_ingredient : "Sin aportaciones"}</li>
-        <li>Fecha de efectividad: {product.effective_time ? product.effective_time : "Sin aportaciones"}</li>
-        <li>No usar: {product.do_not_use ? product.do_not_use : "Sin aportaciones"}</li>
-        <li>Dosis y administración: {product.dosage_and_administration ? product.dosage_and_administration : "Sin aportaciones"}</li>
-        <li>Indicaciones y uso: {product.indications_and_usage ? product.indications_and_usage : "Sin aportaciones"}</li>
-        <li>Propósito: {product.purpose ? product.purpose : "Sin aportaciones"}</li>
-        <li>Advertencias: {product.warnings ? product.warnings : "Sin aportaciones"}</li>
-        <li>Almacenamiento: {product.storage_and_handling ? product.storage_and_handling : "Sin aportaciones"}</li>
-        <li>Preguntas o comentarios: {product.questions ? product.questions : "Sin aportaciones"}</li>
-        <li>parar de usar: {product.stop_use ? product.stop_use : "Sin aportaciones"}</li>
-      </ul>
+      <h2>{product.openfda?.brand_name}</h2>
+      <List
+            sx={{ width: '100%', bgcolor: 'background.default'}}
+            component="ul"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+              <ListSubheader component="div" id="nested-list-subheader">
+                Información del Producto
+              </ListSubheader>
+            }
+      >
+        <ListItemText>
+          <Typography component="span" color="primary">Ingredientes Activos: </Typography> {tryShow(product.active_ingredient)}
+        </ListItemText>
+        <ListItemText>
+          <Typography component="span" color="primary">Fecha de efectividad: </Typography> {tryShow(product.effective_time)}
+        </ListItemText>
+        <ListItemText>
+          <Typography component="span" color="primary">No usar: </Typography> {tryShow(product.do_not_use)}
+        </ListItemText>
+        <ListItemText>
+          <Typography component="span" color="primary">Dosis y administración: </Typography> {tryShow(product.dosage_and_administration)}
+        </ListItemText>
+        <ListItemText>
+          <Typography component="span" color="primary">Indicaciones y uso: </Typography> {tryShow(product.indications_and_usage)}
+        </ListItemText>
+        <ListItemText>
+          <Typography component="span" color="primary">Propósito:  </Typography> {tryShow(product.purpose)}
+        </ListItemText>
+        <ListItemText>
+          <Typography component="span" color="primary">Advertencias:  </Typography> {tryShow(product.warnings)}
+        </ListItemText>
+        <ListItemText>
+          <Typography component="span" color="primary">Almacenamiento: </Typography> {tryShow(product.storage_and_handling)}
+        </ListItemText>
+        <ListItemText>
+          <Typography component="span" color="primary">Preguntas o comentarios: </Typography> {tryShow(product.questions)}
+        </ListItemText>
+        <ListItemText>
+          <Typography component="span" color="primary">Parar de usar: </Typography> {tryShow(product.stop_use)}
+        </ListItemText>
+      </List>
     </article>
   );
 }
